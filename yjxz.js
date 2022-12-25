376d573313fa8b67ca3868585a29a2076a0ee339
Amiking <63540385+Amiking@users.noreply.github.com>
2022-12-25 12:20:18 +0800

    Create yjxz.js

---
+++ yjxz.js
@@ -0,0 +1,24 @@
+/******************************
+
+脚本功能：异界修真creak
+特别说明：尝试破解等级、战力
+软件版本：1.0.0
+脚本作者：mmm
+更新时间：2022-12-25
+使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/game\.aidalan\.com\/v1 url script-response-body https://raw.githubusercontent.com/Amiking/quanx/new/main/yjxz.js
+
+[mitm] 
+
+hostname = game.aidalan.com
+
+
+*******************************/
+
+
+var 'verify': {'rele_level': 999}
